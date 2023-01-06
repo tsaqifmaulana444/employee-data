f72f15f40c681b3c010bc4c43c8df1873d9045a5
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-01-06 20:02:39 +0700

    post method

--- working.py
+++ working.py
@@ -1,17 +1,26 @@
 import random
 import os
 import json
+from typing import Optional, Literal
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
-EMPLOYEE_FILE = "employee.json"
-EMPLOYEE_DATABASE = ["Lee Kwan Htet", "Piotr Szczcrazscny", "Miguel Fernandes", "Wazni Mouruba"]
+class EmployeeModel(BaseModel):
+    name: str
+    salary: float
+    origin: str
+    desc: str
+    gender: Literal["Man", "Woman"]
+    id: Optional[int] =  random.randint(0, 10000)
+
+EMPLOYEE_FILE = "employees.json"
 
 if os.path.exists(EMPLOYEE_FILE):
     with open(EMPLOYEE_FILE, "r", encoding="UTF-8") as f:
         EMPLOYEE_DATABASE = json.load(f)
-        
 
 @app.get('/')
 async def home():
@@ -33,8 +42,21 @@ async def rand_employee():
     return {"employee" : random.choice(EMPLOYEE_DATABASE)}
 
 @app.post("/add-employee")
-async def add_employee (employee: str):
-    EMPLOYEE_DATABASE.append(employee)
+async def add_employee (employee: EmployeeModel):
+    employee.id = random.randint(0, 10000)
+    parse_employee = jsonable_encoder(employee)
+    EMPLOYEE_DATABASE.append(parse_employee)
     with open(EMPLOYEE_FILE, "w", encoding="UTF-8") as F:
         json.dump(EMPLOYEE_DATABASE, F)      
-    return {"message" : f"{employee} successfully inserted"}
+    return {
+        "message" : f"{employee.name} data successfully inserted", 
+        "id" : employee.id
+    }
+    
+@app.get("/get-employee")
+async def get_book(id_emp: int):
+    for employee in EMPLOYEE_DATABASE:
+        if employee["id"] == id_emp:
+            return employee
+    
+    raise HTTPException(404, f"Employee with ID {id_emp} not found")
